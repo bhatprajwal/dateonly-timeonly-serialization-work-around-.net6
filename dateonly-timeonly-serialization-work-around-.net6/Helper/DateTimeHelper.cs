114c55892a71d043868e6e6072f0dc364e59bcc6
Bhat <prajwal.neelkod@gmail.com>
2022-06-06 23:02:25 +0530

    DateOnly & TimeOnly Deserialization
    Solution Simplified with the DateOnly and TimeOnly Converters

---
+++ dateonly-timeonly-serialization-work-around-.net6/Helper/DateTimeHelper.cs
@@ -0,0 +1,21 @@
+﻿using dateonly_timeonly_serialization_work_around_.net6.Converters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace dateonly_timeonly_serialization_work_around_.net6.Helper;
+
+public class DateTimeHelper
+{
+	public static JsonSerializerOptions JsonSerializerOptions
+	{
+		get
+		{
+			var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+			options.Converters.Add(new JsonStringEnumConverter());
+			options.Converters.Add(new DateOnlyConverter());
+			options.Converters.Add(new TimeOnlyConverter());
+			return options;
+		}
+	}
+}
--- dateonly-timeonly-serialization-work-around-.net6/Program.cs
+++ dateonly-timeonly-serialization-work-around-.net6/Program.cs
@@ -1,15 +1,10 @@
 ﻿using dateonly_timeonly_serialization_work_around_.net6.Helper;
 using dateonly_timeonly_serialization_work_around_.net6.Models;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 var inputFilePath = @"Json\Employee.json";
 
 var jsonInput = File.ReadAllText(inputFilePath);
-var employee = JsonConvert.DeserializeObject<Employee>(jsonInput);
-if (employee != null)
-{
-    employee.DateOfBirth = ConversionHelper.GetDateOnly(jsonInput);
-    employee.TimeOfBirth = ConversionHelper.GetTimeOnly(jsonInput);
-}
+var employee = JsonSerializer.Deserialize<Employee>(jsonInput, DateTimeHelper.JsonSerializerOptions);
 
 Console.WriteLine("DateOnly and TimeOnly values bounded successfully!");
